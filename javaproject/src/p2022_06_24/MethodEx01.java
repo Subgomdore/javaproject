444efaf82c83092053b57b3120ed664d491f2147
admin <admin@NoteBook-PC>
2022-06-24 11:28:46 +0900

    22

---
+++ javaproject/src/p2022_06_24/MethodEx01.java
@@ -0,0 +1,25 @@
+package p2022_06_24;
+
+// 메소드 : 여라깆 코드를 묶은 놓은 것
+// 메소드를 사용하는 이유는 코드를 재사용하기 위해서 사용된다.
+
+public class MethodEx01 
+
+//사용자 정의 메소드
+//사용자 정의 메소드는 프로그래머가 직접 호출해야만 실행된다.
+{
+	public static void check() 	// static 이 붙어있는 메소드를 정적 메소드라고 부른다.
+	{
+		System.out.println("함수 호출 성공");
+	}
+	
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+		
+
+		
+
+	}
+
+}
