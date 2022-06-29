d0a806b238fbb523a736dcc06444304a14959cdd
admin <admin@NoteBook-PC>
2022-06-29 15:02:46 +0900

    예제파일

---
+++ javaproject/src/p2022_06_29/NoneObject.java
@@ -0,0 +1,9 @@
+package p2022_06_29;
+
+public class NoneObject {
+    static int number = 3;
+    
+    public static void printNumber() {
+        System.out.println("number = " + number);
+    }
+}            
